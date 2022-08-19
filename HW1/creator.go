b7ff36cd63fa51420151cb520351a3c9a6599a2c
alex_under <baranov.al.a@yandex.ru>
2022-08-19 23:43:21 +0700

    HW1

---
+++ HW1/creator.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func createFiles(i, n *int) {
+	defer func() {
+		if v := recover(); v != nil {
+			fmt.Println("recovered", v)
+		}
+	}()
+	err := os.Mkdir("tmp", 0750)
+	if err != nil && !os.IsExist(err) {
+		fmt.Errorf("ошибка создания папки %w", err)
+		return
+	}
+	for *i < *n {
+		file := "./tmp/" + strconv.Itoa(*i) + ".txt"
+		f, err := os.Create(file)
+		if err != nil {
+			fmt.Errorf("ошибка создания файла %w", err)
+		}
+		defer f.Close()
+		*i++
+	}
+}
+
+func main() {
+	defer func() {
+		if v := recover(); v != nil {
+			dt := time.Now()
+			fmt.Println("Время появления ошибки: ", dt.Format("15:04:05"))
+			fmt.Println("recovered", v)
+		}
+	}()
+	i, n := 0, 10
+	for i < n {
+		createFiles(&i, &n)
+	}
+
+}
---
+++ HW1/errTest.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+	"time"
+)
+
+func calc(a, b int, op string) int {
+	var res int
+	switch op {
+	case "+":
+		res = a + b
+	case "-":
+		res = a - b
+	case "*":
+		res = a * b
+	case "/":
+		res = a / b
+	default:
+		fmt.Println("Операция выбрана неверно")
+		os.Exit(1)
+	}
+	return res
+}
+
+type ErrorWithTrace struct {
+	text  string
+	trace string
+}
+
+func New(text string) error {
+	return &ErrorWithTrace{
+		text:  text,
+		trace: string(debug.Stack()),
+	}
+}
+
+func (e *ErrorWithTrace) Error() string {
+	return fmt.Sprintf("error: %s\ntrace:\n%s", e.text, e.trace)
+}
+
+func main() {
+	var err error
+	defer func() {
+		if v := recover(); v != nil {
+			dt := time.Now()
+			fmt.Println("Время появления ошибки: ", dt.Format("15:04:05"))
+			err = New("my error")
+			fmt.Println(err)
+		}
+	}()
+
+	var a, b, res int
+	var op string
+	a = 5
+	op = "/"
+	res = calc(a, b, op)
+	fmt.Println("Результат операции: ", res)
+
+}
